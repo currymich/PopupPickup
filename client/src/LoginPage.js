19be137433f9fe1de45730168d0af1e6a7d5f01c
Mike Curry <michael.curry@autodesk.com>
2020-12-01 00:49:11 -0800

    working login/signup/logout

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,109 +1,107 @@
-import React, {useEffect,useState} from "react"
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 import axios from "axios";
 
+import LoginForm from "./LoginForm";
+
 const defaultValues = {
-  userName: "",
-  userType:""  
-}
+  username: "",
+  userType: "",
+};
 
-  const LoginPage = () => {
-  const [users, setUsers] = useState([])
-  const [inputs, setInputs] = useState(defaultValues)
+const LoginPage = () => {
+  const [users, setUsers] = useState([]);
+  const [inputs, setInputs] = useState(defaultValues);
 
-  const AppUser = ({ userName, userType }) =>
-  (
-          <div className="item">
-              <p>{userName}</p>
-              <p>{userType}</p>
-          </div>
-
-  )
+  const AppUser = ({ username, userType }) => (
+    <div className="item">
+      <p>{username}</p>
+      <p>{userType}</p>
+    </div>
+  );
 
   const getUsers = async () => {
-    const res = await axios.get("/api/users")
-    setUsers(res.data)
-}
+    const res = await axios.get("/api/users");
+    setUsers(res.data);
+  };
 
-const handleSubmit = async (event) => {
-    event.stopPropagation()
-    event.preventDefault()
+  const handleSubmit = async (event) => {
+    event.stopPropagation();
+    event.preventDefault();
 
-    const res = await axios.post("/api/users", inputs)
-    setUsers(res.data)
-    setInputs(defaultValues)
+    const res = await axios.post("/api/users", inputs);
+    setUsers(res.data);
+    setInputs(defaultValues);
 
-    console.log(users)
-}
+    console.log(users);
+  };
 
+  useEffect(() => {
+    getUsers();
+  }, []);
 
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", textDecoration: "underline" }}>
+        Users
+      </h1>
 
-
-useEffect(() => { getUsers() }, [])
-
-    return (
-      <div>
-        <h1 style={{ textAlign: "center", textDecoration: "underline" }}>Users</h1>
-
-        <div className="container">
-                {
-                users.map(p => (
-                    <AppUser
-                    userName={p.userName}
-                    userType={p.userType}
-                    />
-                ))
-                }
-            </div>
-
-            <h1 style={{ textAlign: "center", textDecoration: "underline" }}>Create Account</h1>
-
-        <div
-          className="w3-theme-d3 w3-container"
-          style={{ padding: "25px", borderRadius: "25px", width:"80%", marginLeft:"10%"}}
-        >
-          <h4>Click here to Login</h4>
-
-          <form onSubmit={handleSubmit}>
-
-                    <input
-                        type="text"
-                        placeholder="User Name"
-                        value={inputs.userName}
-                        onChange={e => setInputs({ ...inputs, userName: e.target.value })}
-                        className="w3-input"
-                        style={{ borderRadius: "25px" }}
-                    />
-
-                    <br />
-
-                    <input
-                        type="text"
-                        placeholder="User Type"
-                        value={inputs.userType}
-                        onChange={e => setInputs({ ...inputs, userType: e.target.value })}
-                        className="w3-input"
-                        style={{ borderRadius: "25px" }}
-                    />
-
-                    <br />
-
-                    <input
-                        type="submit"
-                        value="Submit"
-                        className="w3-theme-d1 w3-btn"
-                        style={{ borderRadius: "25px", width: "100%" }}
-                    />
-              </form>
-        </div>
-
-
-
-
-
-
+      <div className="container">
+        {users.map((p) => (
+          <AppUser username={p.username} userType={p.userType} />
+        ))}
       </div>
-    )
-}
+
+      <h1 style={{ textAlign: "center", textDecoration: "underline" }}>
+        Create Account
+      </h1>
+
+      <div
+        className="w3-theme-d3 w3-container"
+        style={{
+          padding: "25px",
+          borderRadius: "25px",
+          width: "80%",
+          marginLeft: "10%",
+        }}
+      >
+        <h4>Click here to Login</h4>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="User Name"
+            value={inputs.username}
+            onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+            className="w3-input"
+            style={{ borderRadius: "25px" }}
+          />
+
+          <br />
+
+          <input
+            type="text"
+            placeholder="User Type"
+            value={inputs.userType}
+            onChange={(e) => setInputs({ ...inputs, userType: e.target.value })}
+            className="w3-input"
+            style={{ borderRadius: "25px" }}
+          />
+
+          <br />
+
+          <input
+            type="submit"
+            value="Submit"
+            className="w3-theme-d1 w3-btn"
+            style={{ borderRadius: "25px", width: "100%" }}
+          />
+        </form>
+      </div>
+
+      <LoginForm />
+    </div>
+  );
+};
 
 export default LoginPage;
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,60 +1,101 @@
-import React, {useState} from 'react';
-import LoginPage from './LoginPage';
-import Home from './Home';
-import ProductsPage from './ProductsPage';
-import Event from './event';
+import React, { useState } from "react";
+import { useAuthState } from "./AuthProvider";
+import LoginPage from "./LoginPage";
+import Home from "./Home";
+import ProductsPage from "./ProductsPage";
+import Event from "./event";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
-import './css/Nav.css';
-import './css/homePage.css';
+import "./App.css";
+import "./css/Nav.css";
+import "./css/homePage.css";
 
+const NavBar = () => {
+  const { user, logout } = useAuthState();
 
+  const [NavDisplay, setNavDisplay] = useState("none");
+  const [width, setWidth] = useState("0%");
 
-const NavBar = () =>{
+  const w3_open = () => {
+    setNavDisplay("block");
+    setWidth("25%");
+  };
 
-        const [NavDisplay, setNavDisplay] = useState("none")
-        const [width, setWidth] = useState("0%")
-
-
-const w3_open = () => {
-  setNavDisplay("block")
-  setWidth("25%")
-}
-  
   const w3_close = () => {
-    setNavDisplay("none")
-    setWidth("0%")
-  }
+    setNavDisplay("none");
+    setWidth("0%");
+  };
 
+  const style = {
+    display: NavDisplay,
+    width: width,
+  };
+  return (
+    <div>
+      <Router>
+        <div
+          className="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left"
+          style={style}
+        >
+          <button
+            onClick={w3_close}
+            className="3-bar-item w3-button w3-large w3-hover-opacity"
+          >
+            Close
+          </button>
+          <Link to="/" className="w3-bar-item w3-button w3-hover-opacity">
+            Home
+          </Link>
+          <Link
+            to="/ProductsPage/"
+            className="w3-bar-item w3-button w3-hover-opacity"
+          >
+            Vendor/Products
+          </Link>
+          <Link to="/Event/" className="w3-bar-item w3-button w3-hover-opacity">
+            Events
+          </Link>
+        </div>
 
-      
-      const style={
-        display:NavDisplay,
-        width:width
-     }
-        return (       
-<div>
-
- <Router>
-  <div className="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left" style={style} >
-   <button onClick={w3_close} className="3-bar-item w3-button w3-large w3-hover-opacity">Close</button>
-     <Link to="/" className="w3-bar-item w3-button w3-hover-opacity">Home</Link>
-     <Link to="/ProductsPage/" className="w3-bar-item w3-button w3-hover-opacity">Vendor/Products</Link>
-     <Link to="/Event/" className="w3-bar-item w3-button w3-hover-opacity">Events</Link>
-   </div>
-
-  <div className="NavBar">
-    <button className="NavBarIcon" onClick={w3_open}>☰</button>
-    <img style={{height:"50px",marginLeft:"30px",marginTop:"5px"}} onClick={()=><Link to="/"></Link>} src="https://i.postimg.cc/GtKMqRdq/popin-Head.jpg" alt="Untitled-Artwork" border="0"/>
-    <Link style={{float: "right",height:"40px",marginTop:"5px",marginRight:"10px",backgroundColor:"rgb(88,166, 255)",borderRadius: "10px"}} to="/LoginPage/" className="w3-bar-item w3-button w3-hover-opacity">Login</Link>
-  </div>
-  <Route exact path="/" component={Home} />
-  <Route path="/LoginPage" component={LoginPage} />
-  <Route path="/ProductsPage" component={ProductsPage} />
-  <Route path="/Event" component={Event} />
-</Router>
-</div>
-        )
-}
+        <div className="NavBar">
+          <button className="NavBarIcon" onClick={w3_open}>
+            ☰
+          </button>
+          <img
+            style={{ height: "50px", marginLeft: "30px", marginTop: "5px" }}
+            onClick={() => <Link to="/"></Link>}
+            src="https://i.postimg.cc/GtKMqRdq/popin-Head.jpg"
+            alt="Untitled-Artwork"
+            border="0"
+          />
+          {user && (
+            <div
+              style={{ display: "flex", float: "right", alignItems: "center" }}
+            >
+              <h4 style={{ float: "right" }}>Logged in: {user.username}</h4>
+              <button onClick={logout}>Logout</button>
+            </div>
+          )}
+          <Link
+            style={{
+              float: "right",
+              height: "40px",
+              marginTop: "5px",
+              marginRight: "10px",
+              backgroundColor: "rgb(88,166, 255)",
+              borderRadius: "10px",
+            }}
+            to="/LoginPage/"
+            className="w3-bar-item w3-button w3-hover-opacity"
+          >
+            Login
+          </Link>
+        </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/LoginPage" component={LoginPage} />
+        <Route path="/ProductsPage" component={ProductsPage} />
+        <Route path="/Event" component={Event} />
+      </Router>
+    </div>
+  );
+};
 export default NavBar;
-
